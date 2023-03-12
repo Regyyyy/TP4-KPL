aa52426723a4e25d7b4f41d7f5e1c4d1e200c13e
Regyyyy <regyrahman@gmail.com>
2023-03-13 00:32:31 +0700

    Menambahkan implementasi dengan table-driven

--- TPMOD4_1302213117_RegyRenandaRahman/TPMOD4_1302213117_RegyRenandaRahman/Program.cs
+++ TPMOD4_1302213117_RegyRenandaRahman/TPMOD4_1302213117_RegyRenandaRahman/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodePos kode = new KodePos();
+        Console.WriteLine("Kelurahan : Batununggal");
+        Console.WriteLine("Kode Pos : "+kode.getKodePos(KodePos.Kelurahan.Batununggal));
+    }
+}
+
+public class KodePos
+{
+    public enum Kelurahan { Batununggal, Kujangsari, Menggel, 
+        Wates, Cijaura, Jatisari, Margasari, Sekejati, 
+        Kebonwaru, Maleer, Semoja }
+    public int getKodePos(Kelurahan kelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40287,
+        40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodePos[(int)kelurahan];
+    }
+}
