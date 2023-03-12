3beaa9cdaa02977ce67b0d2b9c1da54a90381be3
Regyyyy <regyrahman@gmail.com>
2023-03-13 01:06:44 +0700

    Menambahkan implementasi dengan state-based  construction

--- TPMOD4_1302213117_RegyRenandaRahman/TPMOD4_1302213117_RegyRenandaRahman/Program.cs
+++ TPMOD4_1302213117_RegyRenandaRahman/TPMOD4_1302213117_RegyRenandaRahman/Program.cs
@@ -2,9 +2,14 @@
 {
     private static void Main(string[] args)
     {
+        //Table-Driven
         KodePos kode = new KodePos();
         Console.WriteLine("Kelurahan : Batununggal");
         Console.WriteLine("Kode Pos : "+kode.getKodePos(KodePos.Kelurahan.Batununggal));
+        //State-Based Construction
+        Console.WriteLine("================");
+       DoorMachine door = new DoorMachine();
+        door.Pintu();
     }
 }
 
@@ -18,5 +23,29 @@ public class KodePos
         int[] kodePos = { 40266, 40287, 40267, 40256, 40287,
         40286, 40286, 40286, 40272, 40274, 40273 };
         return kodePos[(int)kelurahan];
+    }
+}
+
+public class DoorMachine
+{
+    enum State { Terkunci, Terbuka };
+    public void Pintu()
+    {
+        State state = State.Terkunci;
+        string[] pesan = { "Pintu terkunci", "Pintu tidak terkunci" };
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Kondisi : "+pesan[(int)state]);
+            Console.Write("Perintah : ");
+            string perintah = Console.ReadLine();
+            if (perintah == "KunciPintu")
+            {
+                state = State.Terkunci;
+            } else if (perintah == "BukaPintu")
+            {
+                state = State.Terbuka;
+            }
+        }
+        Console.WriteLine("Kondisi : " + pesan[(int)state]);
     }
 }
